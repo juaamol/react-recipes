9d0faa8c84d2f543d2082ae66a530d1c5aa3ebdc
juaamol <jambulud@gmail.com>
2019-08-15 18:03:54 +0200

    navbar centered

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const search = (props) => (
-    <div>Hello search page</div>
+    <Form inline className="justify-content-center mt-sm-2">
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
 );
 
 export default search;
