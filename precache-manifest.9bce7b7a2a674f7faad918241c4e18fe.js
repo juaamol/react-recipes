f8cc63f79ca2a21c067434722286a98728f47d83
juaamol <jambulud@gmail.com>
2019-08-19 18:46:58 +0200

    Updates

---
+++ precache-manifest.9bce7b7a2a674f7faad918241c4e18fe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d872abbcf3db470d4867b9cf6a18662",
+    "url": "/react-recipes/index.html"
+  },
+  {
+    "revision": "622c6ccee70a47f40593",
+    "url": "/react-recipes/static/css/main.abb197f9.chunk.css"
+  },
+  {
+    "revision": "b5ada9e5705e7f0436b4",
+    "url": "/react-recipes/static/js/2.8fbd2f23.chunk.js"
+  },
+  {
+    "revision": "622c6ccee70a47f40593",
+    "url": "/react-recipes/static/js/main.31c3ebae.chunk.js"
+  },
+  {
+    "revision": "48d0c4905a49a0f21abd",
+    "url": "/react-recipes/static/js/runtime~main.ec7dcf41.js"
+  },
+  {
+    "revision": "538e82e2e7d60876f4694b2c008f5257",
+    "url": "/react-recipes/static/media/dessert.538e82e2.jpg"
+  },
+  {
+    "revision": "54c35b08ac26e3d98823d1163918d468",
+    "url": "/react-recipes/static/media/salad-bowl-3.54c35b08.png"
+  },
+  {
+    "revision": "21974392650962eb8c3051123ec43a5b",
+    "url": "/react-recipes/static/media/salad.21974392.jpg"
+  },
+  {
+    "revision": "fd4aeef504f32192339780e5c649bae3",
+    "url": "/react-recipes/static/media/sea-food.fd4aeef5.jpg"
+  },
+  {
+    "revision": "fe1337164bca000f546ea09f3d4568eb",
+    "url": "/react-recipes/static/media/table.fe133716.jpg"
+  }
+]);
